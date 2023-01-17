5834dd0cd3a6b6379f4d400c16a37da065ab3b05
Dolgor <budatsyrenova.dolgor@mail.ru>
2023-01-17 16:40:29 +0800

    done

--- src/index.js
+++ src/index.js
@@ -35,11 +35,35 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
-};
-
-function decode(expr) {
-    // write your solution here
-}
+    ' ': ' '
+  };
+  
+  function decode(expr) {
+    let word=[],letter='',k=expr.length%10,exp=expr.slice(k),j=0;
+    if(k!=0){j=1;word[0]=expr.slice(0,k);}
+    for(let i=0;i<exp.length/10;i++,j++){
+      word[j]=exp.slice((i)*10,(i+1)*10);
+    }
+    for(let i=0;i<word.length;i++){
+      letter=word[i];
+      word[i]='';
+      for(let s=0;s<letter.length;s=s+2){
+        if(letter[s]+letter[s+1]=='10') 
+          word[i]=word[i]+'.';
+        else if(letter[s]+letter[s+1]=='11') 
+          word[i]=word[i]+'-';
+        else if(letter[s]+letter[s+1]=='**') {
+          word[i]=' ';
+          continue;
+        }
+      }
+    }
+    letter='';
+    for(let i=0;i<word.length;i++){
+      letter=letter+MORSE_TABLE[word[i]];
+    }
+    return letter;
+  }
 
 module.exports = {
     decode
